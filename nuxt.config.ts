fee9dd494bfa367dbea3389720de429d6b70fb70
落櫻ゆきな <testuseroozx@gmail.com>
2025-03-07 21:30:13 +0800

    🔧 build(default port): change server listen host.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-   devServer: {
-    host: '0.0.0.0',
-    port: 5173,
-  }
-
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    devServer: {
+        host: '0.0.0.0',
+        port: 5173
+    }
 })
