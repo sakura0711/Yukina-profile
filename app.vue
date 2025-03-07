004543414b3d950e93b0993af162323b7219e145
落櫻ゆきな <testuseroozx@gmail.com>
2025-03-08 01:46:51 +0800

    🔧 build(auto-animate, tailwindcss, vueuse): Add Module.

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div>
+        this is test Web.
+        <NuxtRouteAnnouncer />
+        <NuxtWelcome />
+    </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
-    devtools: { enabled: true },
+    devtools: {
+        enabled: true,
+
+        timeline: {
+            enabled: true
+        }
+    },
+
     devServer: {
         host: '0.0.0.0',
         port: 5173
-    }
+    },
+
+    modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@formkit/auto-animate', '@nuxt/icon']
 })
