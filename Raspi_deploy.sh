f47730c4be027874c1b973d164ee9c79bdb214bf
落櫻ゆきな <testuseroozx@gmail.com>
2025-03-09 15:52:21 +0800

    ✨ feat(Raspi_deploy): 修改 `顏色` 和 `git 同步`.

--- Raspi_deploy.sh
+++ Raspi_deploy.sh
@@ -1,20 +1,28 @@
 set -e
 
-echo "git repo 同步中"
+# 設定顏色
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+RESET='\033[0m'  # Reset顏色
+
+# 輸出顏色化訊息
+echo -e "${CYAN}git repo 同步中...${RESET}"
 git status 
 git pull
 
-echo "同步 Nuxt 檔案..."
+echo -e "${YELLOW}同步 Nuxt 檔案...${RESET}"
 sudo chown -R sakura:sakura /mnt/smbshare/yukina-profile/.output
 sudo chmod -R 777 /mnt/smbshare/yukina-profile/.output
 sudo rsync -avh --delete /mnt/smbshare/yukina-profile/.output/. /var/www/nuxt/
 
-echo "重啟 PM2 Nuxt 應用..."
+echo -e "${GREEN}重啟 PM2 Nuxt 應用...${RESET}"
 pm2 delete nuxt-app
 pm2 start /var/www/nuxt/server/index.mjs --name nuxt-app
 pm2 save
 
-echo "重新啟動 Nginx..."
+echo -e "${CYAN}重新啟動 Nginx...${RESET}"
 sudo systemctl restart nginx
 
-echo "部署完成！"
+echo -e "${GREEN}部署完成！${RESET}"
