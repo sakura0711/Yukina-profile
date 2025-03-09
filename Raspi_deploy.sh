057ee238b78e35948f3eacce25f0c41a01b024bd
SakuraYukina <testuseroozx@gmail.com>
2025-03-09 15:43:27 +0800

    fix(file path): 修復路徑名稱產生的錯誤

--- Raspi_deploy.sh
+++ Raspi_deploy.sh
@@ -1,10 +1,18 @@
+set -e
+
+echo "git repo 同步中"
+git status 
+git pull
 
 echo "同步 Nuxt 檔案..."
-rsync -av --delete /mnt/smbshare/yukin-profile/.output/ /var/www/nuxt/
+sudo chown -R sakura:sakura /mnt/smbshare/yukina-profile/.output
+sudo chmod -R 777 /mnt/smbshare/yukina-profile/.output
+sudo rsync -avh --delete /mnt/smbshare/yukina-profile/.output/. /var/www/nuxt/
 
 echo "重啟 PM2 Nuxt 應用..."
 pm2 delete nuxt-app
 pm2 start /var/www/nuxt/server/index.mjs --name nuxt-app
+pm2 save
 
 echo "重新啟動 Nginx..."
 sudo systemctl restart nginx
