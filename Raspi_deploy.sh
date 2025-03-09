4b025b430525d106cefa690a514be996816bb0df
落櫻ゆきな <testuseroozx@gmail.com>
2025-03-09 16:07:46 +0800

    🐞 fix(.gitignore, Raspi_deploy(echo)): 修改 .gitignore (.output) & Raspi_deploy 中的 `echo` 命令.
    Raspi_deploy 中有設定  git repo 同步 .output，減少將檔案從 windows 中複製過去的步驟。

--- Raspi_deploy.sh
+++ Raspi_deploy.sh
@@ -8,21 +8,21 @@ RED='\033[0;31m'
 RESET='\033[0m'  # Reset顏色
 
 # 輸出顏色化訊息
-echo -e "${CYAN}git repo 同步中...${RESET}"
+echo -e "${CYAN}> git repo 同步中 ============================================${RESET}"
 git status 
 git pull
 
-echo -e "${YELLOW}同步 Nuxt 檔案...${RESET}"
+echo -e "${YELLOW}> 同步 Nuxt 檔案 ============================================${RESET}"
 sudo chown -R sakura:sakura /mnt/smbshare/yukina-profile/.output
 sudo chmod -R 777 /mnt/smbshare/yukina-profile/.output
 sudo rsync -avh --delete /mnt/smbshare/yukina-profile/.output/. /var/www/nuxt/
 
-echo -e "${GREEN}重啟 PM2 Nuxt 應用...${RESET}"
+echo -e "${GREEN}> 重啟 PM2 Nuxt 應用 ==========================================${RESET}"
 pm2 delete nuxt-app
 pm2 start /var/www/nuxt/server/index.mjs --name nuxt-app
 pm2 save
 
-echo -e "${CYAN}重新啟動 Nginx...${RESET}"
+echo -e "${CYAN}> 重新啟動 Nginx ===============================================${RESET}"
 sudo systemctl restart nginx
 
-echo -e "${GREEN}部署完成！${RESET}"
+echo -e "${GREEN}> 部署完成！===================================================${RESET}"
