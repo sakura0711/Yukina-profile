05883f3a2600d3a2b262197d3c25557d7effab3d
落櫻 ゆきな <testuseroozx@gmail.com>
2025-07-07 16:38:40 +0800

    🌈 style(header & footer ): 刪除不必要的樣式。

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,15 +3,13 @@
 <template>
     <div>
         <!-- header 固定 -->
-        <LayoutHeaderItem class="fixed left-0 top-0 z-50 block w-full shadow-md"></LayoutHeaderItem>
+        <LayoutHeaderItem class="fixed left-0 top-0 z-50 block w-full p-8 backdrop-blur-md"></LayoutHeaderItem>
 
         <!-- 中間顯示內容(類似 <router-view>) -->
         <slot></slot>
 
         <!-- footer 固定 -->
-        <LayoutFooterItem
-            class="fixed bottom-0 left-0 z-50 w-full bg-neutral-500/50 p-8 text-white shadow-md backdrop-blur-md"
-        ></LayoutFooterItem>
+        <LayoutFooterItem class="fixed bottom-0 left-0 z-50 w-full"></LayoutFooterItem>
     </div>
 </template>
 
