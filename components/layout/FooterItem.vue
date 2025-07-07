10623ce6b0a854ebbc3a8b508c3296a04aada955
落櫻 ゆきな <testuseroozx@gmail.com>
2025-07-07 16:32:18 +0800

    ✨ feat(Footer--link ): 刪除 link 區塊，將 link 移動到 header.

--- components/layout/FooterItem.vue
+++ components/layout/FooterItem.vue
@@ -1,28 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="flex h-10 items-center justify-center gap-4">
-        <a href="https://github.com/sakura0711" class="flex items-center justify-center gap-2" target="_blank">
-            <Icon name="skill-icons:github-dark" size="2em" />
-            <i>GitHub</i>
-        </a>
-        <a
-            href="https://discordapp.com/users/565550082016149515"
-            class="flex items-center justify-center gap-2"
-            target="_blank"
-        >
-            <Icon name="skill-icons:discord" size="2em" />
-            <i>Discord</i>
-        </a>
-        <a
-            href="https://mail.google.com/mail/?view=cm&fs=1&to=testuseroozx@gmail.com&su=&body="
-            target="_blank"
-            class="flex items-center justify-center gap-2"
-        >
-            <Icon name="skill-icons:gmail-light" size="2em" />
-            <i>Gmail</i>
-        </a>
-    </div>
+    <div class=""></div>
 </template>
 
 <style scoped></style>
